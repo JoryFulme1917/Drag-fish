2586dc664c269aa3bcdf0a1ec58e99fa7479086e
Jake <NurkinJK@hendrix.edu>
2025-02-06 09:28:46 -0600

    Player Movement Implemented

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(transform.position.x, pos.y);
+        if (pos.y == 4) {
+            pos.y = 4;
+        }
+        if (pos.y == -4){
+            pos.y = -4;
+        }
+    }
+}
